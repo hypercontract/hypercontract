0e7b7d884c0da49b3bb70275cd683f53d87786e9
Marvin Luchs <marvin.luchs@holisticon.de>
2019-12-12 11:33:29 +0100

    chore(hypershop): add modules for homepage, orders, products, shopping cart and user profile

--- apps/hypershop/src/app/app.module.ts
+++ apps/hypershop/src/app/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { HomepageModule } from './homepage/homepage.module';
+import { OrdersModule } from './orders/orders.module';
+import { ProductsModule } from './products/products.module';
 import { ProfileModule } from './profile';
+import { ShoppingCartModule } from './shopping-cart/shopping-cart.module';
+import { USerProfileModule } from './user-profile/user-profile.module';
 
 @Module({
     imports: [
-        ProfileModule
+        HomepageModule,
+        OrdersModule,
+        ProductsModule,
+        ProfileModule,
+        ShoppingCartModule,
+        USerProfileModule
     ]
 })
-export class AppModule {    }
+export class AppModule {}
---
+++ apps/hypershop/src/app/homepage/homepage.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { HomepageController } from './homepage.controller';
+
+@Module({
+    controllers: [
+    ]
+})
+export class HomepageModule {}
