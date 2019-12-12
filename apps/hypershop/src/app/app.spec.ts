62115e43096b207501009d9233a4e9509207645e
Marvin Luchs <marvin.luchs@holisticon.de>
2019-12-12 20:07:00 +0100

    test(hypershop): add e2e tests for JSON responses

---
+++ apps/hypershop/src/app/app.spec.ts
@@ -0,0 +1,130 @@
+import { toJsonSchemaId } from '@hypercontract/profile';
+import { INestApplication } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import * as Ajv from 'ajv';
+import * as request from 'supertest';
+import { orderMocks } from '../test/orders.mock';
+import { productMocks } from '../test/products.mock';
+import { AppModule } from './app.module';
+import { MediaType } from './content-negotiation';
+import { shop } from './profile/namespaces';
+import { schemas } from './profile/schemas';
+
+const ajv = new Ajv({
+    multipleOfPrecision: 2,
+    schemas: schemas.getAll()
+        .filter(schema => schema.targetType === MediaType.Json)
+        .map(schema => JSON.parse(schema.schemaDefinition))
+});
+
+const toMatchInstanceSchema = (concept: string, response: request.Response) => {
+    const valid = ajv.validate(
+        toJsonSchemaId(shop(concept), MediaType.Json),
+        response.body
+    );
+    if (!valid) {
+        throw new Error(`${ajv.errorsText()}\n\n${JSON.stringify(response.body, undefined, 4)}`);
+    }
+}
+
+describe('hypershop', () => {
+    let app: INestApplication;
+
+    const productMockId = productMocks[0]._id;
+    const orderMockId = orderMocks[0]._id;
+
+    beforeAll(async () => {
+
+        const module = await Test.createTestingModule({
+            imports: [AppModule]
+        })
+            .compile();
+
+        app = module.createNestApplication();
+        await app.init();
+    });
+
+    it(`GET /`, () => {
+        return request(app.getHttpServer())
+            .get(`/`)
+            .expect(200)
+            // .expect(responseToMatch('ApiRoot'));
+            .expect(response => {
+                toMatchInstanceSchema('ApiRoot', response)
+            });
+    });
+
+    it(`GET /products`, () => {
+        return request(app.getHttpServer())
+            .get(`/products`)
+            .expect(200)
+            // .expect(responseToMatch('SearchResults'));
+            .expect(response => {
+                toMatchInstanceSchema('SearchResults', response)
+            });
+    });
+
+    it(`GET /products/${productMockId}`, () => {
+        return request(app.getHttpServer())
+            .get(`/products/${productMockId}`)
+            .expect(200)
+            // .expect(responseToMatch('Product'));
+            .expect(response => {
+                toMatchInstanceSchema('Product', response)
+            });
+    });
+
+    it(`GET /products?queryString=pizza`, () => {
+        return request(app.getHttpServer())
+            .get(`/products`)
+            .expect(200)
+            // .expect(responseToMatch('SearchResults'));
+            .expect(response => {
+                toMatchInstanceSchema('SearchResults', response)
+            });
+    });
+
+    it(`GET /shoppingCart`, () => {
+        return request(app.getHttpServer())
+            .get(`/shoppingCart`)
+            .expect(200)
+            // .expect(responseToMatch('ShoppingCart'));
+            .expect(response => {
+                toMatchInstanceSchema('ShoppingCart', response)
+            });
+    });
+
+    it(`GET /orders`, () => {
+        return request(app.getHttpServer())
+            .get(`/orders`)
+            .expect(200)
+            // .expect(responseToMatch('OrderHistory'));
+            .expect(response => {
+                toMatchInstanceSchema('OrderHistory', response)
+            });
+    });
+
+    it(`GET /orders/${orderMockId}`, () => {
+        return request(app.getHttpServer())
+            .get(`/orders/${orderMockId}`)
+            .expect(200)
+            // .expect(responseToMatch('Order'));
+            .expect(response => {
+                toMatchInstanceSchema('Order', response)
+            });
+    });
+
+    it(`GET /userProfile`, () => {
+        return request(app.getHttpServer())
+            .get(`/userProfile`)
+            .expect(200)
+            // .expect(responseToMatch('UserProfile'));
+            .expect(response => {
+                toMatchInstanceSchema('UserProfile', response)
+            });
+    });
+
+    afterAll(async () => {
+        await app.close();
+    });
+});
--- libs/profile/src/lib/builder/schema/schema.ts
+++ libs/profile/src/lib/builder/schema/schema.ts
@@ -5,31 +5,37 @@ import { toQuads } from '../quad';
 
 interface SchemaMap {
     [conceptUri: string]: {
-        [schemaType: string]: Schema;
+        [targetType: string]: Schema;
     };
 }
 
 export class Schemas {
-    private schemas: SchemaMap = {};
+    private schemas: Schema[] = [];
+    private conceptSchemas: SchemaMap = {};
 
     constructor(schemas: Schema[]) {
         schemas.forEach(schema => this.add(schema));
     }
 
     get(conceptUri: string): Schema[] {
-        return values(this.schemas[conceptUri]);
+        return values(this.conceptSchemas[conceptUri]);
+    }
+
+    getAll() {
+        return [...this.schemas];
     }
 
     private add(schema: Schema) {
-        if (isUndefined(this.schemas[schema.conceptUri])) {
-            this.schemas[schema.conceptUri] = {};
+        if (isUndefined(this.conceptSchemas[schema.conceptUri])) {
+            this.conceptSchemas[schema.conceptUri] = {};
         }
 
-        if (!isUndefined(this.schemas[schema.conceptUri][schema.targetType])) {
+        if (!isUndefined(this.conceptSchemas[schema.conceptUri][schema.targetType])) {
             throw new Error(`A schema of type ${schema.targetType} has already been defined for ${schema.conceptUri}`);
         }
 
-        this.schemas[schema.conceptUri][schema.targetType] = schema;
+        this.schemas = [...this.schemas, schema];
+        this.conceptSchemas[schema.conceptUri][schema.targetType] = schema;
     }
 }
 
