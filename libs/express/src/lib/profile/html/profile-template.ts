fb7ece55c0767cd6e13eafe1583695ee348eabe3
Marvin Luchs <marvin.luchs@holisticon.de>
2019-12-15 13:22:10 +0100

    fix(express): scale correctly on mobile

--- libs/express/src/lib/profile/html/profile-template.ts
+++ libs/express/src/lib/profile/html/profile-template.ts
@@ -2,6 +2,7 @@ export const template = `<!DOCTYPE html>
 <html>
     <head>
         <title><%= title %></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.17.1/themes/prism.css" />
         <style>
