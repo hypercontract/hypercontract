fea7b8ce887efa5b2e5d6dba3f0ad72f083768ad
Marvin Luchs <marvin.luchs@holisticon.de>
2019-12-16 21:08:17 +0100

    fix(hypershop): add EntryPoint type to API root resource

--- apps/hypershop/src/app/api-root/api-root.json-ld.ts
+++ apps/hypershop/src/app/api-root/api-root.json-ld.ts
@@ -1,10 +1,14 @@
+import { hyper } from '@hypercontract/profile';
 import { shop } from '../profile/namespaces';
 import { getApiRootUri, getCatalogSearchUri, getOrdersRootUri, getShoppingCartRootUri, getUserProfileRootUri } from '../routing';
 
 export function toJsonLdApiRoot() {
     return {
         '@id': getApiRootUri(),
-        '@type': shop('ApiRoot'),
+        '@type': [
+            shop('ApiRoot'),
+            hyper('EntryPoint')
+        ],
         [shop('searchCatalog')]: { '@id':getCatalogSearchUri() },
         [shop('shoppingCart')]: { '@id':getShoppingCartRootUri() },
         [shop('orderHistory')]: { '@id':getOrdersRootUri() },
