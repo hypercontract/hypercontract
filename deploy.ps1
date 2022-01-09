0b3995395c4a6ab6bde84582d0fd34bfd0d78879
Marvin Luchs <marvin.luchs@holisticon.de>
2022-01-09 11:55:30 +0100

    feat: add docker container for docs

--- deploy.ps1
+++ deploy.ps1
@@ -3,6 +3,10 @@ Remove-Item .\dist\ -Recurse -Force
 npm run build -- hypershop --prod
 npm run build -- hypercontract --prod
 
+docker build -t hypercontract/docs:latest -f .\Dockerfile.docs .
+docker tag hypercontract/docs hypercontract.azurecr.io/docs
+docker push hypercontract.azurecr.io/docs
+
 docker build -t hypercontract/hypercontract:latest -f .\Dockerfile.hypercontract .
 docker tag hypercontract/hypercontract hypercontract.azurecr.io/hypercontract
 docker push hypercontract.azurecr.io/hypercontract
